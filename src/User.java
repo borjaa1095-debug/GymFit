7338d7ba8d2ae2cbba490c939ee0fa87c5f6666d
Zeus Amaya <zeussamaya@gmail.com>
2026-05-13 10:38:56 +0200

    Atributos y getter y setters en usuario creados

--- src/User.java
+++ src/User.java
@@ -1,2 +1,48 @@
-public class User {
+public abstract class User {
+
+    private String username;
+    private String email;
+    private int id;
+
+    private String password;
+
+    public User(String username, String email, int id) {
+        this.username = username;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
